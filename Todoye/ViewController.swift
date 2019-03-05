9810ad0a348e072cbdd3c8b73e455102b0b09234
Daniil Shvedov <mr.shvedov@icloud.com>
2019-03-05 21:10:27 +0300

    Initial Commit

---
+++ Todoye/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoye
+//
+//  Created by Даниил Шведов on 05/03/2019.
+//  Copyright © 2019 Daniil Shvedov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
