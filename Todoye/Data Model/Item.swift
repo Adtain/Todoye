f0f74a0b95747f927559b761de2be6a782746183
Daniil Shvedov <mr.shvedov@icloud.com>
2019-04-01 21:10:30 +0300

    Convert to MVC design pattern

---
+++ Todoye/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoye
+//
+//  Created by Даниил Шведов on 01/04/2019.
+//  Copyright © 2019 Daniil Shvedov. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
